6f0bf1616f5bcffbca01b5704a1ed5385b07e8d9
Aarti Parmar <aarti.parmar@contis.com>
2023-03-01 16:59:01 +0530

    Rename project

---
+++ go-openapi/controllers/docs.go
@@ -0,0 +1,18 @@
+//	Course Api Document:
+//	 version: V.0.0.1
+//	 title: Course Api
+//	Schemes: http, https
+//	Host: localhost:7000
+//	BasePath: api/
+//	Produces:
+//	  - application/json
+//
+// securityDefinitions:
+//
+//	apiKey:
+//	  type: apiKey
+//	  in: header
+//	  name: authorization
+//
+// swagger:meta
+package controllers
